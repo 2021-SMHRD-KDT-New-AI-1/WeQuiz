e5f2ac44b4115e6f2acf814c884472e5da688273
mongerado <lte7506@naver.com>
2021-12-02 17:46:18 +0900

    xml페이지 추가

---
+++ app/src/main/java/com/hjh/wequiz/MissionMapActivity.java
@@ -0,0 +1,14 @@
+package com.hjh.wequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MissionMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_map2);
+    }
+}
