bd516cfc11c95aa5b255f6831e07615dff97db39
HamGildo <ham5477@naver.com>
2021-12-07 11:50:08 +0900

    login_로그인 상태 유지 기능

--- app/src/main/java/com/hjh/wequiz/LoginActivity.java
+++ app/src/main/java/com/hjh/wequiz/LoginActivity.java
@@ -6,7 +6,6 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -97,6 +96,7 @@ public class LoginActivity extends AppCompatActivity {
                             if (status.equals("success")) {
 
                                 Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                PreferenceManager.setString(mContext,"mem_id", id);
 
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                 startActivity(intent);
