a5a687306f7855cfed153cd1f750cb2a484606fb
mongerado <lte7506@naver.com>
2021-12-07 11:59:33 +0900

    MainActivity_버튼, 페이지이동 완료

--- app/src/main/java/com/hjh/wequiz/QuizLoadingActivity.java
+++ app/src/main/java/com/hjh/wequiz/QuizLoadingActivity.java
@@ -10,5 +10,6 @@ public class QuizLoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_loading);
+
     }
 }
