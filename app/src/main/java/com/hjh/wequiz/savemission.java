95863fc804e1d1541240bc6d925f23ada70be253
jiho7605 <dlqogns7605@naver.com>
2021-12-14 18:20:32 +0900

    mapActivity_마커클릭시팝업및저장교체

--- app/src/main/java/com/hjh/wequiz/savemission.java
+++ app/src/main/java/com/hjh/wequiz/savemission.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 
@@ -15,20 +16,11 @@ public class savemission extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savemission);
-        View savemis_back = findViewById(R.id.savemis_back);
         View btn_savemis = findViewById(R.id.btn_savemis);
         View btn_changemis = findViewById(R.id.btn_changemis);
         requestWindowFeature( Window.FEATURE_NO_TITLE );
 
 
-
-
-        savemis_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
         btn_changemis.setOnClickListener(new View.OnClickListener() {
             @Override
